4562cf909a89e7170921c70dd3ad932441eae6dc
pryhmez <edmundisaac16@gmail.com>
2021-02-20 18:50:06 +0100

    api route

--- src/pages/Apps.js
+++ src/pages/Apps.js
@@ -34,12 +34,17 @@ const Apps = () => {
       text:
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
     },
-   ]);
+  ]);
   return (
     <div>
       <Navigation />
-      <div className="container">
-        <div className="row">
+      <div className="container" style={{ height: "100vh" }}>
+        <div className="row"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.assign('http://negst.us-east-1.elasticbeanstalk.com/')
+          }}
+        >
           {serv.map((service, i) => (
             <Card
               key={i}
@@ -47,6 +52,11 @@ const Apps = () => {
               icon={service.icon}
               hd={service.hd}
               text={service.subtext}
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.assign('http://negst.us-east-1.elasticbeanstalk.com/')
+              }
+              }
             />
           ))}
         </div>
